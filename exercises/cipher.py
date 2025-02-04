381e6cf50c4a6c7f0a917dfcea1ebc5af4c805df
blackzarifa <rafaelirades@gmail.com>
2025-02-04 20:49:39 -0300

    Defined caesar function

--- exercises/cipher.py
+++ exercises/cipher.py
@@ -1,17 +1,23 @@
 text = "Hello Worldxyz"
 shift = 3
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-cipher = ""
 
-for i in text:
-    charIdx = alphabet.find(i.lower())
-    if charIdx == -1:
-        cipher += i
 
-    if charIdx >= len(alphabet) - shift:
-        charIdx -= len(alphabet)
+def caesar(text, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    cipher = ""
 
-    cipherChar = alphabet[charIdx + shift]
-    cipher += cipherChar
+    for i in text:
+        charIdx = alphabet.find(i.lower())
+        if charIdx == -1:
+            cipher += i
 
-print(cipher)
+        if charIdx >= len(alphabet) - shift:
+            charIdx -= len(alphabet)
+
+        cipherChar = alphabet[charIdx + shift]
+        cipher += cipherChar
+
+    return cipher
+
+
+print(caesar(text, shift))
