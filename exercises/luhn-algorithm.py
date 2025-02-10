a23048ba329e06ab98d4ab5378fabe31a94140c7
blackzarifa <rafaelirades@gmail.com>
2025-02-09 21:23:07 -0300

    feat sum of odd digits

--- exercises/luhn-algorithm.py
+++ exercises/luhn-algorithm.py
@@ -1,9 +1,24 @@
+def verifyCardNumber(number):
+    oddSum = sumOddNumbers(number)
+    print(oddSum)
+
+
+def sumOddNumbers(number):
+    sum = 0
+    oddNumbers = number[::-1][::2]
+
+    for i in oddNumbers:
+        sum += int(i)
+
+    return sum
+
+
 def main():
     cardNumber = '4111-1111-4555-1142'
     translation = str.maketrans({'-': '', ' ': ''})
     translatedNumber = cardNumber.translate(translation)
 
-    print(translatedNumber)
+    verifyCardNumber(translatedNumber)
 
 
 main()
