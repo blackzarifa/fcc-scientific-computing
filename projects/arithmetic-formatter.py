c65bef0049424f4e5ae2db4e26a814fb4792c9b1
blackzarifa <rafaelirades@gmail.com>
2025-02-14 17:17:47 -0300

    feat main lines

--- projects/arithmetic-formatter.py
+++ projects/arithmetic-formatter.py
@@ -6,9 +6,15 @@ def arithmetic_arranger(problems, show_answers=False):
     for p in problems:
         splitProblems.append(p.split(' '))
 
+    firstLine = ''
+    secondLine = ''
+    dashLine = ''
+    answerLine = ''
+
     for p in splitProblems:
         len1 = len(p[0])
         len2 = len(p[2])
+
         if p[1] != '+' and p[1] != '-':
             print("Error: Operator must be '+' or '-'.")
             break
@@ -19,9 +25,26 @@ def arithmetic_arranger(problems, show_answers=False):
             print('Error: Numbers cannot be more than four digits.')
             break
 
+        maxLen = max(len1, len2) + 2
+
+        for _ in range(maxLen - len1):
+            firstLine += ' '
+        for _ in range(maxLen - len2):
+            secondLine += ' '
+
+        firstLine += p[0]
+        secondLine += p[2]
+
+        for _ in range(maxLen):
+            dashLine += '-'
+
+        print(firstLine)
+        print(secondLine)
+        print(dashLine)
+
 
 def main():
-    arithmetic_arranger(["32a + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+    arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
 
 
 main()
