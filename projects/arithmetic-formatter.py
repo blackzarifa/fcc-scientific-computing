7a18e0f987bceb1f9f09386fda3f12ae57bc541b
blackzarifa <rafaelirades@gmail.com>
2025-02-13 22:46:57 -0300

    feat early returns

--- projects/arithmetic-formatter.py
+++ projects/arithmetic-formatter.py
@@ -6,11 +6,20 @@ def arithmetic_arranger(problems, show_answers=False):
     for p in problems:
         splitProblems.append(p.split(' '))
 
-    print(splitProblems)
+    for p in splitProblems:
+        if p[1] != '+' or p[1] != '-':
+            print("Error: Operator must be '+' or '-'.")
+            break
+        if not p[0].isdigit() or not p[2].isdigit():
+            print('Error: Numbers must only contain digits.')
+            break
+        if len(p[0]) > 4 or len(p[2]) > 4:
+            print('Error: Numbers cannot be more than four digits.')
+            break
 
 
 def main():
-    arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+    arithmetic_arranger(["32a + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
 
 
 main()
