c56be917939b94004c896125d48707bfb0945f5e
blackzarifa <rafaelirades@gmail.com>
2025-02-16 11:55:58 -0300

    feat constraints

--- exercises/password-generator.py
+++ exercises/password-generator.py
@@ -1,22 +1,37 @@
 import secrets
 import string
+import re
 
 
-def generate_password(length):
+def generate_password(length, nums, special_chars, uppercase, lowercase):
     letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
     all_chars = letters + digits + symbols
 
-    pw = ''
-    for _ in range(length):
-        pw += secrets.choice(all_chars)
+    while True:
+        pw = ''
+        for _ in range(length):
+            pw += secrets.choice(all_chars)
+
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),
+            (special_chars, fr'[{symbols}]'),
+        ]
+
+        if all(
+            constraint <= len(re.findall(pattern, pw))
+            for constraint, pattern in constraints
+        ):
+            break
 
     return pw
 
 
 def main():
-    pw = generate_password(8)
+    pw = generate_password(8, 1, 1, 1, 1)
     print(pw)
 
 
