79730d5e11b6856009a5440b5a8e0fe47557bf6c
blackzarifa <rafaelirades@gmail.com>
2025-02-20 22:38:15 -0300

    feat initial remainder checks

--- exercises/tower-of-hanoi.py
+++ exercises/tower-of-hanoi.py
@@ -5,7 +5,16 @@ rods = {'A': list(range(NUM_DISKS, 0, -1)), 'B': [], 'C': []}
 
 
 def move(n, source, auxiliary, target):
-    pass
+    print(rods)
+
+    for i in range(NUM_MOVES):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f'Move {i + 1} allowed between {source} and {target}')
+        elif remainder == 2:
+            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 
 
 move(NUM_DISKS, 'A', 'B', 'C')
