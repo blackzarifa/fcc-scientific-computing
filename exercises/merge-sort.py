2ad8d6358f5e522f3d38e26bbf9d65027b0baeee
blackzarifa <rafaelirades@gmail.com>
2025-02-23 18:40:00 -0300

    feat merge sort function

---
+++ exercises/merge-sort.py
@@ -0,0 +1,33 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return
+
+    mid = len(array) // 2
+    left = array[:mid]
+    right = array[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    left_idx = 0
+    right_idx = 0
+    sorted_idx = 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            array[sorted_idx] = left[left_idx]
+            left_idx += 1
+        else:
+            array[sorted_idx] = right[right_idx]
+            right_idx += 1
+        sorted_idx += 1
+
+    while left_idx < len(left):
+        array[sorted_idx] = left[left_idx]
+        left_idx += 1
+        sorted_idx += 1
+
+    while right_idx < len(right):
+        array[sorted_idx] = right[right_idx]
+        right_idx += 1
+        sorted_idx += 1
