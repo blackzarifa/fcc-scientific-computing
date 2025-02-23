23345dbc0ecb7d051a192e930f311c274f58744c
blackzarifa <rafaelirades@gmail.com>
2025-02-23 15:49:41 -0300

    refac recursion

--- exercises/tower-of-hanoi.py
+++ exercises/tower-of-hanoi.py
@@ -1,39 +1,18 @@
 NUM_DISKS = 3
-NUM_MOVES = 2**NUM_DISKS - 1
-
 rods = {'A': list(range(NUM_DISKS, 0, -1)), 'B': [], 'C': []}
 
 
-def make_allowed_move(rod1, rod2):
-    forward = False
-    if not rods[rod2]:
-        forward = True
-    elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
-        forward = True
-    if forward:
-        print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
-        rods[rod2].append(rods[rod1].pop())
-    else:
-        print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
-        rods[rod1].append(rods[rod2].pop())
-
-    print(rods, '\n')
-
-
 def move(n, source, auxiliary, target):
+    if n <= 0:
+        return
+
+    move(n - 1, source, target, auxiliary)
+
+    rods[target].append(rods[source].pop())
+
     print(rods, '\n')
 
-    for i in range(NUM_MOVES):
-        remainder = (i + 1) % 3
-        if remainder == 1:
-            print(f'Move {i + 1} allowed between {source} and {target}')
-            make_allowed_move(source, target)
-        elif remainder == 2:
-            print(f'Move {i + 1} allowed between {source} and {auxiliary}')
-            make_allowed_move(source, auxiliary)
-        elif remainder == 0:
-            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
-            make_allowed_move(auxiliary, target)
+    move(n - 1, auxiliary, source, target)
 
 
 move(NUM_DISKS, 'A', 'B', 'C')
