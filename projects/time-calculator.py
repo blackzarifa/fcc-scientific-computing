43bd95819b13915881c914aea45f6b213425931d
blackzarifa <rafaelirades@gmail.com>
2025-02-24 23:33:51 -0300

    feat get hours

--- projects/time-calculator.py
+++ projects/time-calculator.py
@@ -1,4 +1,5 @@
 def add_time(start, duration):
+    # Arrays
     colon_index = start.index(':')
     start_arr = [
         int(start[0:colon_index]),
@@ -7,15 +8,27 @@ def add_time(start, duration):
     ]
     duration_arr = [int(duration[0 : duration.index(':')]), int(duration[-2:])]
 
+    # Minutes
     new_hours = 0
     new_minutes = start_arr[1] + duration_arr[1]
     if new_minutes >= 60:
         new_minutes = new_minutes - 60
         new_hours += 1
 
-    new_time = [new_hours, new_minutes]
+    # Hours
+    new_hours += start_arr[0] + duration_arr[0]
+    extra_periods = 0
+    if new_hours >= 12:
+        extra_periods = new_hours // 12
+        new_hours -= extra_periods * 12
+        if new_hours == 0:
+            new_hours = 12
+
+    new_time = [new_hours, new_minutes, extra_periods]
     return new_time
 
 
 if __name__ == '__main__':
-    print(add_time('10:31 PM', '3:59'))
+    print(add_time('11:00 PM', '1:00'))
+    print(add_time('12:00 PM', '12:00'))
+    print(add_time('12:00 PM', '2:00'))
