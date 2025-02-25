bc0d15ef0b3df930803d35f661b212710fb4cdc8
blackzarifa <rafaelirades@gmail.com>
2025-02-24 22:26:38 -0300

    refac turning strings into ints

--- projects/time-calculator.py
+++ projects/time-calculator.py
@@ -1,11 +1,15 @@
 def add_time(start, duration):
     colon_index = start.index(':')
-    start_arr = [start[0:colon_index], start[colon_index + 1 : -3], start[-2:]]
-    duration_arr = [duration[0 : duration.index(':')], duration[-2:]]
+    start_arr = [
+        int(start[0:colon_index]),
+        int(start[colon_index + 1 : -3]),
+        start[-2:],
+    ]
+    duration_arr = [int(duration[0 : duration.index(':')]), int(duration[-2:])]
 
     new_time = duration_arr
     return new_time
 
 
 if __name__ == '__main__':
-    print(add_time('10:27 PM', '3:30'))
+    print(add_time('10:37 PM', '3:30'))
