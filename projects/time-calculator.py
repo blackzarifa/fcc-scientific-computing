9a7fde0eabb271d99e0fe7c04687e1bbb523d0b5
blackzarifa <rafaelirades@gmail.com>
2025-02-24 22:23:23 -0300

    feat get start and duration as arrays

--- projects/time-calculator.py
+++ projects/time-calculator.py
@@ -1,8 +1,11 @@
 def add_time(start, duration):
-    new_time = start
+    colon_index = start.index(':')
+    start_arr = [start[0:colon_index], start[colon_index + 1 : -3], start[-2:]]
+    duration_arr = [duration[0 : duration.index(':')], duration[-2:]]
 
+    new_time = duration_arr
     return new_time
 
 
 if __name__ == '__main__':
-    print(add_time('10:10 PM', '3:30'))
+    print(add_time('10:27 PM', '3:30'))
