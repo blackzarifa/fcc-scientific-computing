b7ce707d54409bd388cb9e8dfa662917145ef444
blackzarifa <rafaelirades@gmail.com>
2025-03-02 11:51:21 -0300

    feat valid in square

--- exercises/sudoku-solver.py
+++ exercises/sudoku-solver.py
@@ -17,6 +17,15 @@ class Board:
     def valid_in_col(self, col, num):
         return all((self.board[row][col] != num for row in range(9)))
 
+    def valid_in_square(self, row, col, num):
+        row_start = (row // 3) * 3
+        col_start = (col // 3) * 3
+        for cur_row in range(row_start, row_start + 3):
+            for cur_col in range(col_start, col_start + 3):
+                if self.board[cur_row][cur_col] == num:
+                    return False
+        return True
+
 
 PUZZLE = [
     [0, 0, 2, 0, 0, 8, 0, 0, 0],
