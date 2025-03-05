3f6d799b88bc2e58d930bf47f7e7fcf1d07cbb6d
blackzarifa <rafaelirades@gmail.com>
2025-03-05 10:16:43 -0300

    feat deposit

--- projects/budget-app.py
+++ projects/budget-app.py
@@ -1,6 +1,23 @@
 class Category:
-    pass
+    def __init__(self, category_name):
+        self.name = category_name
+        self.value = 0
+        self.ledger = []
+
+    def _add_to_ledger(self, amount, description):
+        self.ledger.append({'amount': amount, 'description': description})
+        print(self.ledger)
+
+    def deposit(self, value):
+        self.value += value
+        self._add_to_ledger(value, 'deposit')
 
 
 def create_spend_chart(categories):
     pass
+
+
+if __name__ == '__main__':
+    test = Category('Test')
+    test.deposit(1000)
+    print(test.name)
