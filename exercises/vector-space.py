6383ba24b1d743012452b32ab6830792c2f4ea71
blackzarifa <rafaelirades@gmail.com>
2025-03-07 22:46:05 -0300

    feat r3 vector

--- exercises/vector-space.py
+++ exercises/vector-space.py
@@ -8,3 +8,9 @@ class R2Vector:
 
     def __str__(self):
         return str(tuple(getattr(self, i) for i in vars(self)))
+
+
+class R3Vector(R2Vector):
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
+        self.z = z
