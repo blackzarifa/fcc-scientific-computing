1b454cfb872e2ad5333787b67aaa1921bcbd9584
blackzarifa <rafaelirades@gmail.com>
2025-03-07 22:45:26 -0300

    feat r2 vector class

---
+++ exercises/vector-space.py
@@ -0,0 +1,10 @@
+class R2Vector:
+    def __init__(self, *, x, y):
+        self.x = x
+        self.y = y
+
+    def norm(self):
+        return sum(val**2 for val in vars(self).values()) ** 0.5
+
+    def __str__(self):
+        return str(tuple(getattr(self, i) for i in vars(self)))
