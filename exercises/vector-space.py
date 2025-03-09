3a412b9a9278e2a957ca69b69b84cf2eb50cb54b
blackzarifa <rafaelirades@gmail.com>
2025-03-09 11:17:08 -0300

    def cross method for R3

--- exercises/vector-space.py
+++ exercises/vector-space.py
@@ -64,3 +64,20 @@ class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+
+    def cross(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+
+        kwargs = {
+            'x': self.y * other.z - self.z * other.y,
+            'y': self.z * other.x - self.x * other.z,
+            'z': self.x * other.y - self.y * other.x,
+        }
+        return self.__class__(**kwargs)
+
+
+v1 = R3Vector(x=2, y=3, z=1)
+v2 = R3Vector(x=1, y=2, z=2)
+print(v1, v2)
+print(v1.cross(v2))
