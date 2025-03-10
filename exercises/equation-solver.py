3a8aabbc83d14356878df564481a27d994c68c98
blackzarifa <rafaelirades@gmail.com>
2025-03-10 11:41:44 -0300

    feat initial commit with basic init_subclass

---
+++ exercises/equation-solver.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class Equation(ABC):
+    degree: int
+
+    def __init__(self, *args):
+        pass
+
+    def __init_subclass__(cls):
+        if not hasattr(cls, "degree"):
+            raise AttributeError(
+                f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
+            )
+
+    @abstractmethod
+    def solve(self):
+        pass
+
+    @abstractmethod
+    def analyze(self):
+        pass
+
+
+class LinearEquation(Equation):
+    degree = 1
+
+    def solve(self):
+        pass
+
+    def analyze(self):
+        pass
