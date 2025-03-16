71e720eef3909ec7a55782145167dbb297dd5ec6
blackzarifa <rafaelirades@gmail.com>
2025-03-16 17:11:19 -0300

    feat initial Projectile class

--- exercises/projectile-trajectory-calculator.py
+++ exercises/projectile-trajectory-calculator.py
@@ -4,3 +4,12 @@ GRAVITATIONAL_ACCELERATION = 9.81
 PROJECTILE = "∙"
 x_axis_tick = "T"
 y_axis_tick = "⊣"
+
+
+class Projectile:
+    __slots__ = ('__speed', '__height', '__angle')
+
+    def __init__(self, speed, height, angle):
+        self.__speed = speed
+        self.__height = height
+        self.__angle = math.radians(angle)
