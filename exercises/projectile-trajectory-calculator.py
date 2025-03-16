263e39bbc8155d6c0494333953d74cf4b78fbe43
blackzarifa <rafaelirades@gmail.com>
2025-03-16 17:20:24 -0300

    feat calculate displacement

--- exercises/projectile-trajectory-calculator.py
+++ exercises/projectile-trajectory-calculator.py
@@ -13,3 +13,16 @@ class Projectile:
         self.__speed = speed
         self.__height = height
         self.__angle = math.radians(angle)
+
+    def __calculate_displacement(self):
+        horizontal_component = self.__speed * math.cos(self.__angle)
+        vertical_component = self.__speed * math.sin(self.__angle)
+        squared_component = vertical_component**2
+        gh_component = 2 * GRAVITATIONAL_ACCELERATION * self.__height
+        sqrt_component = math.sqrt(squared_component + gh_component)
+
+        return (
+            horizontal_component
+            * (vertical_component + sqrt_component)
+            / GRAVITATIONAL_ACCELERATION
+        )
