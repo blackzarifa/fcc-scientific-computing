60c92fbf26b07df5d02c4a54c8355ad55f28116a
blackzarifa <rafaelirades@gmail.com>
2025-03-23 11:46:12 -0300

    feat initialize Hat

--- projects/probability-calculator.py
+++ projects/probability-calculator.py
@@ -3,8 +3,16 @@ import random
 
 
 class Hat:
-    pass
+    def __init__(self, **kwargs):
+        self.contents = []
+
+        for key, _ in kwargs.items():
+            self.contents.append(key)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+
+
+if __name__ == '__main__':
+    hat1 = Hat(red=5, orange=4)
