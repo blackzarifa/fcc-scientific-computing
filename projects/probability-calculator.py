22af1e62cef59addf1a86b18e09f23d4aaebd516
blackzarifa <rafaelirades@gmail.com>
2025-03-23 12:03:31 -0300

    feat draw method

--- projects/probability-calculator.py
+++ projects/probability-calculator.py
@@ -9,10 +9,20 @@ class Hat:
         for key, _ in kwargs.items():
             self.contents.append(key)
 
+    def draw(self, amount):
+        removed_balls = []
+
+        for i in range(amount):
+            index = random.randint(0, len(self.contents) - 1)
+            removed_balls.append(self.contents.pop(index))
+
+        return removed_balls
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
 
 
 if __name__ == '__main__':
-    hat1 = Hat(red=5, orange=4)
+    hat1 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+    hat1.draw(1)
